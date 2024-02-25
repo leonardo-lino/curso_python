352d7a7fc51fe5800635bae9a96eeebe47e5ed5b
Leonardo Lino <leonardo.lino@outlook.com.br>
2024-02-24 22:14:41 -0300

    exercicios_incluidos

---
+++ 71_args.py
@@ -0,0 +1,11 @@
+'''
+args - argumentos não nomeados
+* - *args (empacotamento e desempacotamento)
+'''
+
+x, y, *resto = 1,2,3,4,5,6,7,8,9 #ele empacotou o resto das váriaveis na #variavel "resto"
+print(x,y)
+print(resto)
+
+
+#aula 112
